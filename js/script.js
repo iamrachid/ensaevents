f4241c751acf437f156dd1d83ebb2081fe575c7f
Rachid <rachid-tcs2000@gmail.fr>
2021-10-26 19:17:37 +0100

    done 1.0

--- js/script.js
+++ js/script.js
@@ -1,22 +1,53 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".events", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+    ,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  
+  })
+  
+  var swiper = new Swiper(".clubs", {
+    spaceBetween: 10,
+    autoplay: {
+      delay: 3000,
+      // TODO: this should be false
+      disableOnInteraction: true,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
       640: {
         slidesPerView: 2,
         spaceBetween: 20,
       },
       768: {
-        slidesPerView: 4,
+        slidesPerView: 3,
         spaceBetween: 40,
       },
       1024: {
         slidesPerView: 5,
         spaceBetween: 50,
       },
-    },
-  
-  });
+    }
+  });;
